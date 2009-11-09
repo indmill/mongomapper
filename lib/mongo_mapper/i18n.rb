8bce868bf1b4161cd79305bf0f9fe58cfc47b717
Steven Mohapi-Banks <steve@steve.local>
2009-11-09 20:00:06 +0000

    Changed from human_attribute_name to human_field_name. Feels like a better fit

--- lib/mongo_mapper/i18n.rb
+++ lib/mongo_mapper/i18n.rb
@@ -1,12 +1,12 @@
 module MongoMapper
   module I18n
     
-    def human_attribute_name(field_name, options = {})
+    def human_field_name(field_name, options = {})
       default = []
       default << options.delete(:default)
       default << field_name.to_s.humanize
       default.flatten!
-      options.merge!(:default => default, :scope => [:document, :attributes])
+      options.merge!(:default => default, :scope => [:document, :fields])
       ::I18n.translate(:"#{self.class.name.underscore}.#{field_name.to_s}", options)
     end
 
